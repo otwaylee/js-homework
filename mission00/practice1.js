ecce1f33b743b49e0700b8db7eebc80aade86391
otwaylee <ahyeong858@gmail.com>
2024-06-23 17:34:12 +0900

    1차 과제- 답안

--- mission00/practice1.js
+++ mission00/practice1.js
@@ -29,6 +29,30 @@ const user = {
 // console.log(getValueAtObject(user, 'id')); // asd@naver.com
 // console.log(getValueAtObject(user, 'age')); //해당 키가 존재하지 않습니다. 
 
+
+// 답
+// function isObject(data){
+//   return Object.prototype.toString.call(data).slice(8,-1).toLowerCase() === 'object'
+// }
+// function getValueAtObject(obj,key){
+
+//   if(typeof key !== 'string'){
+//     throw new TypeError('getValueAtObject 함수의 두 번째 인수는 문자 타입 이어야 합니다.')
+//   }
+  
+//   if(isObject(obj)){
+//     if(Object.prototype.hasOwnProperty.call(obj,key)){
+//       return obj[key];
+//     }
+//     else{
+//       throw new Error(`getValueAtObject 함수의 해당 ${key}가 존재하지 않습니다.`);
+//     }
+//   }
+//   else{
+//     throw new TypeError('getValueAtObject 함수의 첫 번째 인수는 객체 타입 이어야 합니다.');
+//   }
+  
+// }
 /*------------------------------------------*/
 
 // 2. 배열에서 특정 인덱스의 값을 안전하게 가져오는 함수 
@@ -75,3 +99,24 @@ const numbers = [10, 20, 30, 40, 50];
 // console.log(getNumberAtArray(numbers, 4)); // 50
 // console.log(getNumberAtArray(numbers, 5)); // Error!
 // console.log(getNumberAtArray(numbers, -1)); // Error!
+
+
+// 답
+// 1. arr 변수가 배열인지 확인하기
+// 2. 0보다 크거나 같음 && 배열의 길이보다 작을 때
+
+// function getNumberAtArray(arr, index){
+
+//   if(Array.isArray(arr)){
+//     if(index >= 0 && index < arr.length){
+//       return arr[index];
+//     }
+//     else{
+//       throw new Error('배열에 없는 index입니다.');
+//     }
+//   }
+//   else{
+//     throw new TypeError('getNumberAtArray 함수의 첫 번째 인수는 배열 타입 이어야 합니다.')
+//   }
+  
+// }
